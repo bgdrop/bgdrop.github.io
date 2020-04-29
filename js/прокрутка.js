9cbb4d8455ac366c94a25adf7569a41ec6766f46
bgdrop <63907019+bgdrop@users.noreply.github.com>
2020-04-29 17:33:59 +0300

    Add files via upload

---
+++ js/прокрутка.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function () {
+    $("#menu").on("click", "a", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({ scrollTop: top }, 1500);
+    });
+});
+
+$(document).ready(function () {
+    $("#knopka").on("click", "a", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({ scrollTop: top }, 1500);
+    });
+});
+
